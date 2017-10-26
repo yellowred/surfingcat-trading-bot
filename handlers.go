d0777efe8decbffd3842718136770ca22d91a5df
Oleg Kubrakov <surfingcat@surfingcat.local>
2017-10-26 11:17:39 +0800

    fetch new ticks one by one 5min candles

--- handlers.go
+++ handlers.go
@@ -133,7 +133,8 @@ func handleTraderStart(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	
-	candleSticks, err := bittrex.GetTicks(market, "thirtyMin")
+	// periods -> ["oneMin", "fiveMin", "thirtyMin", "hour", "day"]
+	candleSticks, err := bittrex.GetTicks(market, "fiveMin")
 	if err != nil {
 		fmt.Println("ERROR OCCURRED: ", err)
 		panic(err)
@@ -143,58 +144,67 @@ func handleTraderStart(w http.ResponseWriter, r *http.Request) {
 	// spot wma cross
 	// buy/sell
 
-	var closes []float64
-	for _, candle := range candleSticks {
-		closes = append(closes, candle.Close)
-	}
 	fmt.Println("Trading started at", time.Now().String())
-	go func(market string, closes *[]float64) {
-		strategyWma(market, closes)
+	go func(market string, candles *bittrex.CandleSticks) {
+		strategyWma(market, candles)
 		for {
 			select {
-				case <-time.After(30 * time.Minute):
-					strategyWma(market, closes)	
+				case <-time.After(30 * time.Second):
+					strategyWma(market, candles)	
 				
 			}
 		}
-	}(market, &closes)
+	}(market, &candleSticks)
 	jsonResponse, _ := json.Marshal("OK")
 	fmt.Fprintf(w, string(jsonResponse))
 }
 
 
-func strategyWma(market string, closes *[]float64) {
+func strategyWma(market string, candles *bittrex.CandleSticks) {
 	fmt.Println("Tick", market, time.Now().String())
-	temp := *closes
-	*closes = temp[len(temp)-1000:] //take 1000 values
-	candleStick, err := bittrex.GetLatestTick(market, "thirtyMin")
+	temp := *candles	
+	candleStick, err := bittrex.GetLatestTick(market, "fiveMin")
 	if err != nil {
 		fmt.Println("Latest stick was not received: ", err)
 	} else {
-		*closes = append(*closes, candleStick.Close)
-	}
+		fmt.Println("Latest tick", temp[len(temp)-1])
+		fmt.Println("New tick", candleStick)
+		if (temp[len(temp)-1].Timestamp != candleStick.Timestamp) {
+			temp = append(temp, *candleStick)
+			*candles = temp[len(temp)-1000:] //take 1000 values
+			fmt.Println("Accepted", len(*candles))
 
-	indicatorData1 := talib.Wma(*closes, 50)
-	indicatorData2 := talib.Wma(*closes, 20)
-
-	if ((indicatorData1[len(indicatorData1)-1] < indicatorData2[len(indicatorData1)-1]) && (indicatorData1[len(indicatorData1)-2] > indicatorData2[len(indicatorData1)-2])) ||	((indicatorData1[len(indicatorData1)-1] > indicatorData2[len(indicatorData1)-1]) && (indicatorData1[len(indicatorData1)-2] < indicatorData2[len(indicatorData1)-2])) {
-		action := "sell"
-		if indicatorData2[len(indicatorData1)-1] - indicatorData2[len(indicatorData1)-2] > 0 {
-			action = "buy"
+			closes := valuesFromCandles(*candles)
+			indicatorData1 := talib.Wma(closes, 50)
+			indicatorData2 := talib.Wma(closes, 20)
+		
+			if ((indicatorData1[len(indicatorData1)-1] < indicatorData2[len(indicatorData1)-1]) && (indicatorData1[len(indicatorData1)-2] > indicatorData2[len(indicatorData1)-2])) ||	((indicatorData1[len(indicatorData1)-1] > indicatorData2[len(indicatorData1)-1]) && (indicatorData1[len(indicatorData1)-2] < indicatorData2[len(indicatorData1)-2])) {
+				action := "sell"
+				if indicatorData2[len(indicatorData1)-1] - indicatorData2[len(indicatorData1)-2] > 0 {
+					action = "buy"
+				}
+				marketSummary, err := bittrex.GetMarketSummary(market)
+				if err != nil {
+					fmt.Println("ERROR OCCURRED: ", err)
+				} else {
+					fmt.Println(marketSummary)
+					fmt.Println("WMA crossed, action:", action, ", price:", marketSummary.Ask)
+				}
+			} else {
+				distance := math.Min(math.Abs(indicatorData2[len(indicatorData2)-1] - indicatorData1[len(indicatorData1)-1]), math.Abs(indicatorData2[len(indicatorData2)-2] - indicatorData1[len(indicatorData1)-2]))
+				fmt.Println("Distance:", distance)
+			}
 		}
-		marketSummary, err := bittrex.GetMarketSummary(market)
-		if err != nil {
-			fmt.Println("ERROR OCCURRED: ", err)
-		} else {
-			fmt.Println(marketSummary)
-			fmt.Println("WMA crossed, action:", action, ", price:", marketSummary.Ask)
-		}
-	} else {
-		distance := math.Min(math.Abs(indicatorData2[len(indicatorData2)-1] - indicatorData1[len(indicatorData1)-1]), math.Abs(indicatorData2[len(indicatorData2)-2] - indicatorData1[len(indicatorData1)-2]))
-		fmt.Println("Distance:", distance)
 	}
 }
 
+func valuesFromCandles(candleSticks bittrex.CandleSticks) []float64 {
+	var closes []float64
+	for _, candle := range candleSticks {
+		closes = append(closes, candle.Close)
+	}
+	return closes
+}
 
 func stringInSlice(a string, list []string) bool {
     for _, b := range list {
