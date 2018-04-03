e229bd762e47e44dae17aa8adcd396e1c3117c61
Oleg <kubrakov@me.com>
2018-04-03 16:42:42 +0800

    Setting up some access

--- tools/passwords.go
+++ tools/passwords.go
@@ -11,14 +11,14 @@ import (
 // How to use: `echo to_be_encrypted | go run tools/passwords.go
 func main() {
 	password, err := ioutil.ReadAll(os.Stdin)
-	password = []byte("AAA")
+	//password = []byte("AAA")
 	log.Println(string(password))
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		panic("Permissions: bcrypt password hashing unsuccessful")
 	}
 	log.Println(string(hash))
-	if bcrypt.CompareHashAndPassword([]byte("$2a$10$nc9E3dp7YQGzTq7Cx8Lpi.Mq98bZ2JqVebGznGax..rN3A1yuYxKa"), []byte("AAA")) != nil {
+	if bcrypt.CompareHashAndPassword([]byte(""), password) != nil {
 		log.Fatalln("bad password")
 	}
 
